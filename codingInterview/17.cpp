38d71b2df7ecc194227c0865e4c223cd34d25841
YMelon <929354023@qq.com>
2018-09-03 11:12:21 +0800

    add 17 problem 2th solution

--- codingInterview/17.cpp
+++ codingInterview/17.cpp
@@ -41,3 +41,51 @@ bool Increment(char* num) {
 
     return isOverflow;
 }
+
+void PrintNum(char* num) {
+    bool isBegin0 = true;
+    int nLen = strlen(num);
+
+    for(int i = 0; i < nLen; ++i){
+	if(isBegin0 && num[i] != '0')
+	    isBegin0 = false;
+
+	if(!isBegin0)
+	    printf("%c", num[i]);
+    }
+
+    printf("\t");
+}
+
+
+
+
+//第二种算法
+void Print1ToMaxOfNDigits(int n) {
+    if(n <= 0)
+	return;
+
+    char* num = new char[n+1];
+    num[n] = '\0';
+    
+    for(int i = 0; i < 10; ++i) {
+	num[0] = i + '0';
+	Print1ToMaxOfNDigitsRecur(num, n, 0);
+    }
+
+    delete[] num;
+}
+
+void Print1ToMaxOfNDigitsRecur(char* num, int len, int index) {
+    if(index == len - 1) {
+	PrintNum(num);
+	return;
+    }
+
+    for(int i = 0; i < 10; ++i) {
+	num[index+1] = i + '0';
+	Print1ToMaxOfNDigitsRecur(num, len, index+1);
+    }
+}
+
+
